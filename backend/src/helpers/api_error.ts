de30e9bf935b0c261de4f40aaa86f914b5124bee
Mateus <mateuscristofori@gmail.com>
2022-12-08 11:59:36 -0300

    Criando novas classes para os erros

--- backend/src/helpers/api_error.ts
+++ backend/src/helpers/api_error.ts
@@ -5,3 +5,21 @@ export class ApiError extends Error{
     this.statusCode = statusCode;
   }
 }
+
+export class BadRequestError extends ApiError {
+  constructor(msg: string) {
+    super(msg, 400);
+  }
+}
+
+export class NotFoundError extends ApiError {
+  constructor(msg: string) {
+    super(msg, 404);
+  }
+}
+
+export class ForbiddenError extends ApiError {
+  constructor(msg: string) {
+    super(msg, 403)
+  }
+}
