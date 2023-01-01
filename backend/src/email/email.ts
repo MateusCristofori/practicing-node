13a77e828a69a797a92a43022cf4e94040358ab7
Mateus <mateuscristofori@gmail.com>
2023-01-01 19:03:11 -0300

    Isolando o envio de email em classe(em andamento)

--- backend/src/email/email.ts
+++ backend/src/email/email.ts
@@ -1,22 +1,31 @@
 import nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
-const transport = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "ajhororow12@gmail.com",
-    pass: "ilizxujnimtzpngd"
+export class Email {
+  private static email: typeof nodemailer;
+
+  static createTransport(): nodemailer.Transporter<SMTPTransport.SentMessageInfo>{
+    return this.email.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "ajhororow12@gmail.com",
+        pass: "ilizxujnimtzpngd"
+      }
+    });
   }
-});
 
-transport.sendMail({
-  from: "Aplicação blog <ajhororow12@gmail.com>",
-  to: "mateuscristofori@gmail.com",
-  subject: "Teste de recuperação de senha",
-  html: "<h1>Recuperação de senha </h1> <p>Envio de email para a recuperação de senha</p>",
-}).then((response) => {
-  console.log("Email enviado com sucesso.");
-}).catch((err) => {
-  console.log("erro ao enviar o email", err);
-});
+  static sendEmail() {
+    this.createTransport().sendMail({
+      from: "Aplicação blog <ajhororow12@gmail.com>",
+      to: "mateuscristofori@gmail.com",
+      subject: "Teste de recuperação de senha",
+      html: "<h1>Recuperação de senha </h1> <p>Envio de email para a recuperação de senha</p>",
+    }).then((response) => {
+      console.log("Email enviado com sucesso.");
+    }).catch((err) => {
+      console.log("erro ao enviar o email", err);
+    });
+  }
+}
