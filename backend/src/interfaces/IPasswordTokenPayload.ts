27b008dc4246e37ce259452cdf58ccce14f96982
Mateus <mateuscristofori@gmail.com>
2023-01-05 19:38:07 -0300

    Resolvendo bug de usuário admin e criando rotas para atualizar e deletar notícias

--- backend/src/interfaces/IPasswordTokenPayload.ts
+++
@@ -1,8 +0,0 @@
-import { JwtPayload } from "jsonwebtoken";
-
-export interface IPasswordTokenPayload extends JwtPayload {
-  user: {
-    id: string,
-    email: string
-  }
-}
--- backend/src/interfaces/IRequestWithRecoveryToken.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from "express";
-import { IPasswordTokenPayload } from "./IPasswordTokenPayload";
-
-export interface IRequestPasswordRecoveryToken extends Request {
-  recoveryToken?: IPasswordTokenPayload
-}
