a471bebf85fab0d592d44ac4fd961b573864fcf1
MateusCristofori <mateuscristofori@gmail.com>
2023-02-26 19:14:48 -0500

    Send emails with nodemailer

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,12 +11,16 @@ export default class UserController {
   async registerNewUserHandler(req: Request, res: Response) {
     const { content }: CreateUserDTO = req.body;
 
-    if(!content) {
-      return res.status(404).json({ error: "Precisa-se passar todas as credenciais." });
+    if (!content) {
+      return res
+        .status(404)
+        .json({ error: "Precisa-se passar todas as credenciais." });
     }
 
-    if(!content.email.includes("@")) {
-      return res.status(400).json({ error: "Deve-se passar o e-mail e senha!" });
+    if (!content.email.includes("@")) {
+      return res
+        .status(400)
+        .json({ error: "Deve-se passar o e-mail e senha!" });
     }
 
     const newUser = await db.user.create({
@@ -26,48 +30,58 @@ export default class UserController {
         password: await generatePasswordHash(content.password),
       },
     });
-   
+
     return res.status(201).json({ newUser });
   }
 
   async userLoginHandler(req: Request, res: Response) {
     const { content }: UserLoginDTO = req.body;
 
-    if(!content) {
-      return res.status(400).json({ error: "Deve-se passar o e-mail e senha!" });
+    if (!content) {
+      return res
+        .status(400)
+        .json({ error: "Deve-se passar o e-mail e senha!" });
     }
 
-    if(!content.email.includes("@")) {
+    if (!content.email.includes("@")) {
       return res.status(400).json({ error: "O email precisa ser válido!" });
     }
 
     const user = await db.user.findFirst({
       where: {
-        email: content.email
-      }
+        email: content.email,
+      },
     });
 
-    if(!user) {
+    if (!user) {
       return res.status(400).json({ error: "E-mail incorreto!" });
     }
-  
+
     const passwordMatch = await bcrypt.compare(content.password, user.password);
-    
-    if(!passwordMatch) {
-      return res.status(400).json({ error: "Senha incorreta!" })
+
+    if (!passwordMatch) {
+      return res.status(400).json({ error: "Senha incorreta!" });
     }
-  
-    const token = jwt.sign({
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email
+
+    const token = jwt.sign(
+      {
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+      },
+      process.env.SECRET as string,
+      {
+        expiresIn: "1d",
       }
-    }, process.env.SECRET as string, {
-      expiresIn: "1d"
-    });
-    
-   const isValidToken = CheckValidateToken.isTokenValid(token);
+    );
+
+    const isValidToken = CheckValidateToken.isTokenValid(token);
+
+    if (!isValidToken) {
+      return res.status(403).json({ error: "Token de autorização inválido." });
+    }
 
     res.status(200).json({
       auth: true,
@@ -76,7 +90,7 @@ export default class UserController {
         name: user.name,
         email: user.email,
       },
-      token
+      token,
     });
   }
 }
